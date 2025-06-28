be6a3d62066f0a1a9bc9ab82e86fdaf49e79cb5f
Utkarsh Shukla <159155698+utkarsh2338@users.noreply.github.com>
2025-06-29 01:40:19 +0530

    Add files via upload
    In this file, I have added a few functionalities:
    1) The user can ask as many questions as they can on the terminal.
    2) It collects the chat's history, so if the user asks a question related to the previous chat, they can get the answer to that also.
    Modules used -> "@google/genai" and "readline-sync" to read from the terminal window.
    Here, thinking is disabled because of a smaller number of tokens.

---
+++ LLM.js
@@ -0,0 +1,34 @@
+import { GoogleGenAI } from "@google/genai";
+import readlineSync from "readline-sync";
+const ai = new GoogleGenAI({ apiKey: "AIzaSyAoevTMQGwK6Yg2gmHa7xtlk6cYPiOxJmU" });
+
+const history = [];
+
+async function chatting() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: history,
+    config: {
+      thinkingConfig: {
+        thinkingBudget: 0, // Disables thinking
+      },
+    }
+  });
+  console.log(response.text);
+}
+async function main() {
+    console.log("Welcome to the chat! Type 'exit' to quit.");
+
+    while (true) {
+        const userInput = readlineSync.question("You: ");
+        if (userInput.toLowerCase() === "exit") {
+            break;
+        }
+        history.push({ role: "user", text: userInput });
+        process.stdout.write("Thinking...\r");
+        await chatting();
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+    }
+}
+main();
